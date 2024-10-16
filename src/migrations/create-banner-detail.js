77c1ab14a575ff3feb163d9194f12d2daaf10afa
Phạm Đức Bình <binhpd@oeg.vn>
2024-10-16 14:17:54 +0700

    update relationship

---
+++ src/migrations/create-banner-detail.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('banner_details', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'products', 
+          key: 'id',    
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      banner_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'banner', 
+          key: 'id',    
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('banner_details');
+  }
+};
---
+++ src/models/bannerdetail.ts
@@ -0,0 +1,28 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize: any, DataTypes: any) => {
+  class BannerDetail extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models: any) {
+      // define association here
+      BannerDetail.belongsTo(models.Banner, { foreignKey: 'banner_id' })
+      BannerDetail.belongsTo(models.Product, { foreignKey: 'product_id' })
+    }
+  }
+  BannerDetail.init(
+    {
+      product_id: DataTypes.INTEGER,
+      banner_id: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: 'BannerDetail',
+      tableName: 'banner_details'
+    }
+  )
+  return BannerDetail
+}
