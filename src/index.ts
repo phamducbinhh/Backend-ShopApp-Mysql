f2374ba7351d18f7c2aca7d16383c12fc04a3150
Phạm Đức Bình <binhpd@oeg.vn>
2024-10-16 11:06:57 +0700

    Update connect Database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('hello world');
+const express = require('express')
+const { connectDatabase } = require('./config/connectDatabase')
+const cors = require('cors')
+const app = express()
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['POST', 'GET', 'PUT', 'DELETE']
+  })
+)
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.use(express.urlencoded({ extended: true }))
+
+const startServer = async () => {
+  try {
+    await connectDatabase()
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App server listening on port: ${process.env.PORT || 4000}`)
+    })
+  } catch (err: any) {
+    console.error('App server error:', err.stack)
+  }
+}
+
+startServer()
