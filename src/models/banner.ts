539df7eaf1c0212afc67662504be166f7af5b2ae
Phạm Đức Bình <binhpd@oeg.vn>
2024-10-16 11:57:15 +0700

    update migration

---
+++ src/models/banner.ts
@@ -0,0 +1,27 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize: any, DataTypes: any) => {
+  class Banner extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models: any) {
+      // define association here
+    }
+  }
+  Banner.init(
+    {
+      name: DataTypes.STRING,
+      image: DataTypes.TEXT,
+      status: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: 'Banner',
+      tableName: 'banner'
+    }
+  )
+  return Banner
+}
---
+++ src/models/news.ts
@@ -0,0 +1,27 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize: any, DataTypes: any) => {
+  class News extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models: any) {
+      // define association here
+    }
+  }
+  News.init(
+    {
+      name: DataTypes.STRING,
+      image: DataTypes.TEXT,
+      content: DataTypes.TEXT
+    },
+    {
+      sequelize,
+      modelName: 'News',
+      tableName: 'news'
+    }
+  )
+  return News
+}
