4d8d32ac2651c8114aa5d43938dd252ffdb5dd95
Phạm Đức Bình <binhpd@oeg.vn>
2024-10-16 16:06:09 +0700

    update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ const productRouter = require('./products.routes')
 const categoryRouter = require('./category.routes')
 const brandRouter = require('./brand.routes')
 const orderRouter = require('./order.routes')
+const orderDetailRouter = require('./orderDetail.routes')
 
 
 const initRoutes = (app: any): void => {
@@ -9,6 +10,7 @@ const initRoutes = (app: any): void => {
   app.use('/api/v1/categories', categoryRouter)
   app.use('/api/v1/brands', brandRouter)
   app.use('/api/v1/orders', orderRouter)
+  app.use('/api/v1/orderDetails', orderDetailRouter)
 }
 
 module.exports = initRoutes
