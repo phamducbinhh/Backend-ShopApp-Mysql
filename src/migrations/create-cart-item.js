779e79b013f25c4b822a96cbb9536c5a15ef1000
Phạm Đức Bình <binhpd@oeg.vn>
2024-10-18 16:01:20 +0700

    update migration

---
+++ src/migrations/create-cart-item.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('carts_items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'products',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      cart_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'carts',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('carts_items');
+  }
+};
---
+++ src/models/cartitem.ts
@@ -0,0 +1,29 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize: any, DataTypes: any) => {
+  class CartItem extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models: any) {
+      // define association here
+      CartItem.belongsTo(models.Product, { foreignKey: 'product_id' })
+      CartItem.belongsTo(models.Cart, { foreignKey: 'cart_id' })
+    }
+  }
+  CartItem.init(
+    {
+      product_id: DataTypes.INTEGER,
+      cart_id: DataTypes.INTEGER,
+      quantity: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: 'CartItem',
+      tableName: 'carts_items'
+    }
+  )
+  return CartItem
+}
