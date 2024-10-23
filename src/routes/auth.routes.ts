2226ff75164953b41bb786db6efac0af3f8b489b
Phạm Đức Bình <binhpd@oeg.vn>
2024-10-23 09:31:19 +0700

    update token

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -8,7 +8,7 @@ const validate = require('../middlewares/validate')
 
 const { verifyRole } = require('../middlewares/jwtMiddleware')
 
-const adminAuth = [verifyRole([ROLE.ADMIN])]
+const adminAuth = [verifyRole([ROLE.ADMIN, ROLE.USER])]
 
 const InsertUserSchema = require('../schema/user/insertUserSchema')
 
@@ -17,7 +17,9 @@ const LoginUserSchema = require('../schema/user/loginUserSchema')
 const router = express.Router()
 
 router.post('/login', validate(LoginUserSchema), AuthController.login)
+
 router.post('/register', validate(InsertUserSchema), AuthController.register)
+
 router.post('/logout', adminAuth, AuthController.logout)
 
 module.exports = router
